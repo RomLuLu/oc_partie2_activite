8efd6a7d5e0b4d3bdc76c1f2155f5fad232b7bbd
RomLuLu <romain.lulu@gmail.com>
2019-01-25 22:12:53 +0100

    Refactoring script manyFonction : ajout fonction ramdom_letter

--- manyFonctionWithRandom.py
+++ manyFonctionWithRandom.py
@@ -1,5 +1,5 @@
-from random import randint
-
+from random import randint, choice
+import string 
 def mix_letters(string):
     """ fonction qui renvoie le mot avec les lettre dans le désordre"""
     l_string = list(string)
@@ -9,11 +9,28 @@ def mix_letters(string):
         index = randint(0, len(l_string)-1)
         letter = l_string.pop(index)
         l_mixed.append(letter)
+    
         c+=1
     return "".join(l_mixed)
 
 
 print(mix_letters("bonjour"))
+
+
+def random_letter():
+    liste_letters = list()
+    for i in range(8):
+        letter = choice(string.ascii_uppercase)
+        liste_letters.append(letter)
+    return '-'.join(liste_letters)
+
+print(random_letter())
+
+    
+
+
+
+
 
 
 
